61acdbf1c3eebfda32dcbf4491981a3ec90da16c
Jorge Carrion <jorge_28cs@hotmail.com>
2020-10-13 00:29:49 -0500

    Vercel Test

--- src/config/index.js
+++ src/config/index.js
@@ -1,13 +1,14 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const config = {
-  dev: process.env.NODE_ENV !== 'production',
+  remoteDB: process.env.REMOTE_DB || false,
+  dev: process.env.NODE_ENV !== "production",
   port: process.env.PORT || 3000,
   cors: process.env.CORS,
   dbUser: process.env.DB_USER,
   dbPassword: process.env.DB_PASSWORD,
   dbHost: process.env.DB_HOST,
-  dbName: process.env.DB_NAME
+  dbName: process.env.DB_NAME,
 };
 
 module.exports = { config };
